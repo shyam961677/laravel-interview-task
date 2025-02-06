4fcb334a2bfac39355c77434446661b67a996c63
Pradeep Kumar <pradeepdev001@gmail.com>
2025-02-06 13:51:19 +0530

    ...

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,17 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            // $table->string('name');
+            // $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('image')->nullable();
-            $table->string('mobile')->nullable();
-            $table->date('date')->nullable(); // e.g. date of birth
+            // $table->string('image')->nullable();
+            // $table->string('mobile')->nullable();
+            // $table->date('date')->nullable(); // e.g. date of birth
             $table->enum('role', ['Admin', 'User'])->default('User');
             $table->timestamps();
         });
+
+        $SQL = 'ALTER TABLE `users` ADD `name` VARBINARY(191) NULL AFTER `id`, ADD `email` VARBINARY(191) NULL AFTER `name`, ADD `image` VARBINARY(191) NULL AFTER `email`, ADD `mobile` VARBINARY(191) NULL AFTER `image`, ADD `date` VARBINARY(191) NULL AFTER `mobile`';
+
+        \DB::statement($SQL);
     }
 
     /**
